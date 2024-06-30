9dc1acf274fd6227258901313168e93d5bf4d5ff
Sultanov Abdurazzoq <abdurazzoqsultanov2208@gmail.com>
2024-06-30 15:35:16 +0500

    Add use client directive to Nexus component
    - Added a timer
    - Progress added , NexusCard, and NexusWords added
    - Responsive managed

--- app/vocabulary/page.tsx
+++ app/vocabulary/page.tsx
@@ -1,5 +1,13 @@
+'use client'
+import Nexus from "@/components/games/nexus"
+import WordGame from "@/components/games/nexusWords"
+
 const VocabularyPage = () => {
-	return <main>Vocabulary</main>
+	return <main>
+
+		<Nexus />
+
+	</main>
 }
 
 export default VocabularyPage
