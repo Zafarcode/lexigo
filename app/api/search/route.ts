7eeb04112e111e27f3f5d6c5e53fc2a40513a8e4
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-09-08 14:31:17 +0500

    fix: api search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,8 +1,8 @@
-import { getPages } from '@/app/source'
+import { grammar } from '@/app/source'
 import { createSearchAPI } from 'next-docs-zeta/search/server'
 
 export const { GET } = createSearchAPI('advanced', {
-	indexes: getPages().map(page => ({
+	indexes: grammar.getPages().map(page => ({
 		title: page.data.title,
 		structuredData: page.data.exports.structuredData,
 		id: page.url,
