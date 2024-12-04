9214d63ebc0e1079d3fbb0066a8160a81cecec59
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2024-12-04 20:34:01 +0500

    fix: progress and disegn upgrade

--- app/dashboard/vocabulary/page.tsx
+++ app/dashboard/vocabulary/page.tsx
@@ -5,15 +5,14 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Progress } from '@/components/ui/progress'
 import { LogoIcon } from '@/components/utils/icons'
 import WordImage from '@/components/vocabulary/word-image'
-import { lessons } from '@/constants/lessons'
 import { cn } from '@/lib/utils'
+import { useProgressStore } from '@/store/use-progress'
 import { motion } from 'framer-motion'
 import { Trophy } from 'lucide-react'
 import Link from 'next/link'
-import { useState } from 'react'
 
 const Vocabulary = () => {
-	const [completedLessons, setCompletedLessons] = useState<number[]>([])
+	const { sections } = useProgressStore()
 
 	return (
 		<section className='py-12'>
@@ -23,67 +22,74 @@ const Vocabulary = () => {
 				</h1>
 
 				<ul className='max-w-4xl mx-auto space-y-8'>
-					{lessons.map((lesson, index) => (
-						<motion.li
-							key={lesson.id}
-							initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50, y: 50 }}
-							animate={{ opacity: 1, x: 0, y: 0 }}
-							transition={{ delay: index * 0.1, duration: 0.5 }}
-							className={cn(
-								'w-full md:w-2/4',
-								index % 2 === 0 ? 'mr-auto' : 'ml-auto'
-							)}
-						>
-							<Card
+					{sections.map((section, index) => {
+						const completedUnits = section.units.filter(
+							(							u: { isCompleted: any }) => u.isCompleted
+						).length
+						const totalUnits = section.units.length
+						const progress = (completedUnits / totalUnits) * 100
+						
+
+						return (
+							<motion.li
+								key={section.id}
+								initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50, y: 50 }}
+								animate={{ opacity: 1, x: 0, y: 0 }}
+								transition={{ delay: index * 0.1, duration: 0.5 }}
 								className={cn(
-									'rounded-lg shadow-md transition-all duration-300 hover:shadow-lg',
-									'dark:bg-[#1c1f26] bg-white text-black dark:text-white'
+									'w-full md:w-2/4',
+									index % 2 === 0 ? 'mr-auto' : 'ml-auto'
 								)}
 							>
-								<CardHeader>
-									<CardTitle className='text-lg font-semibold'>
-										English Module {lesson.id}
-									</CardTitle>
-									<div className='flex items-center relative'>
-										<Progress
-											value={completedLessons.includes(lesson.id) ? 1 : 0}
-											className={cn('h-4 w-full max-w-52 bg-green-100', {
-												'bg-primary': completedLessons.includes(lesson.id),
+								<Card
+									className={cn(
+										'rounded-lg shadow-md transition-all duration-300 hover:shadow-lg',
+										'dark:bg-[#1c1f26] bg-white text-black dark:text-white'
+									)}
+								>
+									<CardHeader>
+										<CardTitle className='text-lg font-semibold'>
+											English Module {section.id}
+										</CardTitle>
+										<div className='flex items-center relative'>
+											<Progress
+												value={progress}
+												className={cn('h-4 w-full max-w-52 bg-green-100', {
+													'bg-primary': progress === 30,
+												})}
+												aria-label={`Progress: ${progress ? '30' : '1'}`}
+											/>
+											<span className='absolute top-1/2 text-sm left-[45%] -translate-x-[45%] md:left-[45%] md:-translate-x-[45%] lg:left-[45%] xl:left-[24%] xl:-translate-x-[22%] lg:-translate-x-[45%] -translate-y-1/2'>
+												{progress.toFixed(0) + ' %'}
+											</span>
+											<Trophy className='w-[34px] h-[30px] text-primary absolute top-[45%] left-[100%] -translate-x-[100%] md:left-[110%] md:-translate-x-[110%] xl:left-[53%] lg:left-[110%] lg:-translate-x-[110%] xl:-translate-x-[53%] -translate-y-1/2 fill-white' />
+										</div>
+									</CardHeader>
+									<CardContent className='flex md:flex-col xl:flex-row justify-between items-center mt-4'>
+										<LogoIcon width={100} height={50} />
+										<Link
+											href={`/dashboard/vocabulary/${section.slug}`}
+											className={cn(buttonVariants({ variant: 'primary' }), {
+												'opacity-50': index > 0 && !progress,
 											})}
-											aria-label={`Progress: ${
-												completedLessons.includes(lesson.id) ? '30' : '1'
-											}`}
-										/>
-										<span className='absolute top-1/2 text-sm left-[45%] -translate-x-[45%] md:left-[45%] md:-translate-x-[45%] lg:left-[45%] xl:left-[22%] xl:-translate-x-[22%] lg:-translate-x-[45%] -translate-y-1/2'>
-											1/30
-										</span>
-										<Trophy className='w-[34px] h-[30px] text-primary absolute top-1/2 left-[100%] -translate-x-[100%] md:left-[110%] md:-translate-x-[110%] xl:left-[53%] lg:left-[110%] lg:-translate-x-[110%] xl:-translate-x-[53%] -translate-y-1/2 fill-white' />
-									</div>
-								</CardHeader>
-								<CardContent className='flex md:flex-col xl:flex-row justify-between items-center mt-4'>
-									<LogoIcon width={100} height={50} />
-									<Link
-										href={`/dashboard/vocabulary/${lesson.slug}`}
-										className={cn(buttonVariants({ variant: 'primary' }), {
-											'opacity-50':
-												index > 0 && !completedLessons.includes(index),
-										})}
-										onClick={() => {
-											if (index === 0 || completedLessons.includes(index)) {
-												setCompletedLessons(prev => [...prev, lesson.id])
-											}
-										}}
-									>
-										{index === 0 || completedLessons.includes(index)
-											? completedLessons.includes(lesson.id)
-												? 'Completed'
-												: 'Continue'
-											: 'Jump Here'}
-									</Link>
-								</CardContent>
-							</Card>
-						</motion.li>
-					))}
+											onClick={() => {
+												if (index > 0 && progress === 0) {
+													return
+												}
+												
+											}}
+										>
+											{index === 0 || progress
+												? progress === 100
+													? 'Completed'
+													: 'Continue'
+												: 'Jump here'}
+										</Link>
+									</CardContent>
+								</Card>
+							</motion.li>
+						)
+                    })}
 				</ul>
 			</div>
 
--- components/vocabulary/section/index.tsx
+++ components/vocabulary/section/index.tsx
@@ -1,35 +1,48 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { ArrowLeft, ChevronLeft } from 'lucide-react'
+import { ArrowLeft, Star, Trophy, Box, Play } from 'lucide-react'
 import { Button, buttonVariants } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { UnitNode } from './unit-node'
-import type { Section as SectionType } from '@/types'
-import Link from 'next/link'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import CircularProgress from './circle'
+import type { Section as SectionType } from '@/types'
 
 interface SectionProps {
 	section: SectionType
 	onUnitClick: (unitId: number) => void
+	unitProgress: Record<number, number>
 }
 
-export function Section({ section, onUnitClick }: SectionProps) {
-	const getUnitPositions = (
-		isRight: boolean
-	): Record<number, { top: string; left: string }> => {
-		return {
-			1: { top: '10%', left: isRight ? '50%' : '50%' },
-			2: { top: '25%', left: isRight ? '60%' : '40%' },
-			3: { top: '45%', left: isRight ? '65%' : '35%' },
-			4: { top: '65%', left: isRight ? '60%' : '40%' },
-			5: { top: '85%', left: isRight ? '50%' : '50%' },
+export function Section({ section, onUnitClick, unitProgress }: SectionProps) {
+	const getUnitPositions = (): Record<
+		number,
+		{ top: string; left: string }
+	> => {
+		const positions: Record<number, { top: string; left: string }> = {}
+		const amplitude = 25 // Amplitude of the wave
+		const spacing = 5 // Spacing between units
+		const startY = 5 // Starting Y position
+		const centerX = 50 // Center X position
+		const totalUnits = 30 // Total number of units
+
+		
+		const frequency = (Math.PI * 2) / (totalUnits - 1) // Frequency
+
+		for (let i = 0; i < totalUnits; i++) { 
+			const y = startY + i * spacing 
+			const x = centerX + amplitude * Math.sin(frequency * i) // X position
+
+			positions[i + 1] = {
+				top: `${y}%`,
+				left: `${x}%`,
+			}
 		}
+
+		return positions
 	}
 
-
-	const isRight = section.id % 2 === 0
-	const unitPositions = getUnitPositions(isRight)
 
 	const getThemeColors = () => {
 		switch (section.theme) {
@@ -38,108 +51,148 @@ export function Section({ section, onUnitClick }: SectionProps) {
 					bg: 'bg-pink-50',
 					header: 'bg-pink-500',
 					button: 'bg-white text-pink-500 hover:bg-pink-50',
-				}
-			case 'pink-dark':
-				return {
-					bg: 'bg-pink-100',
-					header: 'bg-pink-700',
-					button: 'bg-white text-pink-700 hover:bg-pink-50',
-				}
-			case 'purple':
-				return {
-					bg: 'bg-purple-50',
-					header: 'bg-purple-500',
-					button: 'bg-white text-purple-500 hover:bg-purple-50',
+					progress: '#f9a8d4',
 				}
 			case 'blue':
 				return {
 					bg: 'bg-blue-50',
 					header: 'bg-blue-500',
 					button: 'bg-white text-blue-500 hover:bg-blue-50',
+					progress: '#93c5fd',
+				}
+			case 'green':
+				return {
+					bg: 'bg-green-50',
+					header: 'bg-green-500',
+					button: 'bg-white text-green-500 hover:bg-green-50',
+					progress: '#86efac',
+				}
+			default:
+				return {
+					bg: 'bg-gray-50',
+					header: 'bg-gray-500',
+					button: 'bg-white text-gray-500 hover:bg-gray-50',
+					progress: '#d1d5db',
 				}
 		}
 	}
 
 	const colors = getThemeColors()
 
+	const getIcon = (
+		type: 'start' | 'star' | 'chest' | 'trophy' | 'character'
+	) => {
+		switch (type) {
+			case 'start':
+				return <Play className='h-6 w-6' />
+			case 'star':
+				return <Star className='h-6 w-6' />
+			case 'chest':
+				return <Box className='h-6 w-6' />
+			case 'trophy':
+				return <Trophy className='h-6 w-6' />
+			case 'character':
+				return <Star className='h-6 w-6' />
+		}
+	}
+
+	const getUnitThemeColors = (
+		isCompleted: boolean,
+		isLocked: boolean,
+		theme: string
+	) => {
+		switch (theme) {
+			case 'pink':
+				return isCompleted
+					? 'bg-pink-500 text-white'
+					: isLocked
+					? 'bg-gray-300 text-gray-500'
+					: 'bg-pink-100 text-pink-500'
+			case 'blue':
+				return isCompleted
+					? 'bg-blue-500 text-white'
+					: isLocked
+					? 'bg-gray-300 text-gray-500'
+					: 'bg-blue-100 text-blue-500'
+			case 'green':
+				return isCompleted
+					? 'bg-green-500 text-white '
+					: isLocked
+					? 'bg-gray-300 text-gray-500'
+					: 'bg-green-100 text-green-500'
+			default:
+				return isCompleted
+					? 'bg-gray-500 text-white'
+					: isLocked
+					? 'bg-gray-300 text-gray-500'
+					: 'bg-gray-100 text-gray-500'
+		}
+	}
+
+	const unitPositions = getUnitPositions()
+
 	return (
-		<>
-			<Card
-				className={`relative w-full max-w-xl overflow-hidden border-none shadow-none`}
+		<Card
+			className={`relative w-full max-w-4xl border-none shadow-none`}
+		>
+			<div
+				className={`flex flex-col items-center justify-between p-4 rounded-lg ${colors.header} text-white`}
 			>
-				<div
-					className={`flex flex-col items-center justify-between p-4 sticky top-0 rounded-lg ${colors.header} text-white`}
-				>
-					<div className='flex justify-between w-full'>
-						<div className='flex items-center gap-2'>
-							<Link
-								href='/dashboard/vocabulary'
-								className={cn(
-									buttonVariants({ variant: 'ghost', size: 'icon', className: 'text-white hover:bg-transparent/15' })
-								)}
-							>
-								<ArrowLeft className='h-6 w-6' />
-							</Link>
-							<h2 className='text-lg font-bold'>Lesson {section.id}</h2>
-						</div>
-						<Button className={colors.button}>GUIDEBOOK</Button>
+				<div className='flex justify-between w-full'>
+					<div className='flex items-center gap-2'>
+						<Link
+							href='/dashboard/vocabulary'
+							className={cn(
+								buttonVariants({
+									variant: 'ghost',
+									size: 'icon',
+									className: 'text-white hover:bg-transparent/15',
+								})
+							)}
+						>
+							<ArrowLeft className='h-6 w-6' />
+						</Link>
+						<h2 className='text-lg font-bold'>Module {section.id}</h2>
 					</div>
-					<h3 className='text-2xl font-bold text-white'>{section.title}</h3>
+					<Button className={colors.button}>GUIDEBOOK</Button>
 				</div>
-				<div className='relative h-[600px] w-full'>
-					{/* Units */}
-					{section.units.map(unit => (
-						<motion.div
-							key={unit.id}
+				<h3 className='text-2xl font-bold text-white'>{section.title}</h3>
+			</div>
+			<div className='relative h-[2000px] w-full'>
+				{section.units.map(unit => (
+					<motion.div
+						key={unit.id}
+						initial={{ scale: 0.8, opacity: 0 }}
+						animate={{ scale: 1, opacity: 1 }}
+						transition={{ duration: 0.5 }}
+						className='absolute'
+						style={{
+							top: unitPositions[unit.id].top,
+							left: unitPositions[unit.id].left,
+							transform: 'translate(-50%, -50%)',
+						}}
+					>
+						<motion.button
 							initial={{ scale: 0.8, opacity: 0 }}
 							animate={{ scale: 1, opacity: 1 }}
-							transition={{ duration: 0.5 }}
-							className='absolute'
-							style={{
-								top: unitPositions[unit.id]?.top,
-								left: unitPositions[unit.id]?.left,
-								transform: 'translate(-50%, -50%)',
-							}}
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.95 }}
+							onClick={() => !unit.isLocked && onUnitClick(unit.id)}
+							disabled={unit.isLocked}
+							className={`relative flex h-16 w-16 items-center justify-center rounded-full ${getUnitThemeColors(
+								unit.isCompleted,
+								unit.isLocked,
+								section.theme
+							)} shadow-2xl`}
 						>
-							<UnitNode
-								type={unit.type}
-								isCompleted={unit.isCompleted}
-								isLocked={unit.isLocked}
-								onClick={() => onUnitClick(unit.id)}
-								theme={section.theme}
-							/>
-						</motion.div>
-					))}
-				</div>
-			</Card>
-
-			<div className='flex items-center justify-center space-x-4 h-16'>
-				{/* Left text */}
-				<motion.div
-					className='h-px bg-gray-400'
-					initial={{ width: 0 }}
-					animate={{ width: '100px' }}
-					transition={{ duration: 0.8 }}
-				/>
-
-				{/* Text */}
-				<motion.div
-					className='text-gray-400 text-lg'
-					initial={{ opacity: 0, y: -10 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ duration: 0.8, delay: 0.3 }}
-				>
-					{section.title}
-				</motion.div>
-
-				{/* Right text */}
-				<motion.div
-					className='h-px bg-gray-400'
-					initial={{ width: 0 }}
-					animate={{ width: '100px' }}
-					transition={{ duration: 0.8 }}
-				/>
+							<CircularProgress
+								progress={unitProgress[unit.id] || 0}
+								icon={getIcon(unit.type)} theme={colors.progress}							/>
+							
+						</motion.button>
+					</motion.div>
+				))}
 			</div>
-		</>
+		</Card>
 	)
 }
