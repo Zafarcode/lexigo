5a8c252beea66844c91659de222966c96b68dce8
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2025-01-21 22:39:05 +0500

    add: ifreme

--- app/dashboard/grammar/[slug]/page.tsx
+++ app/dashboard/grammar/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { grammar } from '@/app/source'
+import FillInTheBlankGame from '@/components/grammar/fill-in-the-blank'
 import { Separator } from '@/components/ui/separator'
+import { tense } from '@/constants/mock'
 import { createMetadata } from '@/utils/metadata'
 import type { Metadata } from 'next'
 import Link from 'next/link'
@@ -9,40 +11,60 @@ interface Param {
 	slug: string
 }
 
+
 export default function Page({ params }: { params: Param }) {
 	const topic = grammar.getPage([params.slug])
+
+	const topics = tense.find(item => item.name === topic?.data.title)
 
 	if (!topic) {
 		notFound()
 	}
 
 	return (
-		<article className='min-h-screen'>
-			<div className='container py-10'>
-				<div className='flex flex-col md:flex-row gap-5 justify-end items-start'>
-					<Link
-						className='mb-5 md:fixed md:top-38 md:left-40'
-						href='/dashboard/grammar'
-					>
-						<span className='whitespace-nowrap hover:text-primary'>
-							&#8636; Back
-						</span>
-					</Link>
+		<>
+			<article className='min-h-screen'>
+				<div className='container py-10'>
+					<div className='flex flex-col md:flex-row gap-5 justify-end items-start'>
+						<Link
+							className='mb-5 md:fixed md:top-38 md:left-40'
+							href='/dashboard/grammar'
+						>
+							<span className='whitespace-nowrap hover:text-primary'>
+								&#8636; Back
+							</span>
+						</Link>
 
-					<div className='w-full md:w-[calc(100%-200px)]'>
-						<h1 className='text-3xl lg:text-[48px] lg:leading-[56px] font-extrabold text-neonBlue'>
-							{topic.data.title}
-						</h1>
+						<div className='w-full md:w-[calc(100%-200px)]'>
+							<h1 className='text-3xl lg:text-[48px] lg:leading-[56px] font-extrabold text-neonBlue'>
+								{topic.data.title}
+							</h1>
 
-						<Separator className='my-5' />
+							<Separator className='my-5' />
 
-						<article className='prose prose-invert'>
-							<topic.data.exports.default />
-						</article>
+							<article className='prose prose-invert'>
+								<topic.data.exports.default />
+							</article>
+						</div>
 					</div>
 				</div>
+			</article>
+
+			<div className='container mx-auto max-w-3xl'>
+				<div className='relative w-full max-w-2xl mx-auto'>
+					<div className='relative w-full pb-[56.25%] rounded-lg overflow-hidden'>
+						<iframe
+							className='absolute top-0 left-0 w-full h-full rounded-lg'
+							src={`https://www.youtube.com/embed/${topics?.embed ?? ''}`}
+							title='YouTube video player'
+							allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+						></iframe>
+					</div>
+				</div>
+
+				<FillInTheBlankGame questions={topics?.questions ?? []} />
 			</div>
-		</article>
+		</>
 	)
 }
 
---
+++ constants/mock.tsx
@@ -0,0 +1,346 @@
+export const tense = [
+	{
+		id: 1,
+		name: "To be fe'li",
+		embed: 'n-uTwzzVnsg',
+		questions: [
+			{
+				id: 1,
+				question: 'She ___ a doctor.',
+				answer: 'is',
+				options: ['am', 'is', 'are'],
+			},
+			{
+				id: 2,
+				question: 'They ___ at the park yesterday.',
+				answer: 'were',
+				options: ['was', 'were', 'is'],
+			},
+		],
+	},
+	{
+		id: 2,
+		name: 'Present Simple',
+		embed: 'IL_008MAgwQ',
+		questions: [
+			{
+				id: 1,
+				question: 'He ___ football every Sunday.',
+				answer: 'plays',
+				options: ['play', 'plays', 'playing'],
+			},
+			{
+				id: 2,
+				question: 'They ___ to school by bus.',
+				answer: 'go',
+				options: ['go', 'goes', 'going'],
+			},
+		],
+	},
+	{
+		id: 3,
+		name: 'Present Continuous',
+		embed: 'MsrT1z4EuOg',
+		questions: [
+			{
+				id: 1,
+				question: 'She ___ a book right now.',
+				answer: 'is reading',
+				options: ['reads', 'read', 'is reading'],
+			},
+			{
+				id: 2,
+				question: 'We ___ to the party.',
+				answer: 'are going',
+				options: ['go', 'goes', 'are going'],
+			},
+		],
+	},
+	{
+		id: 4,
+		name: 'Irregular Verbs',
+		embed: '379UO6V8niE',
+		questions: [
+			{
+				id: 1,
+				question: "The past tense of 'go' is ___.",
+				answer: 'went',
+				options: ['goed', 'went', 'gone'],
+			},
+			{
+				id: 2,
+				question: "The past tense of 'eat' is ___.",
+				answer: 'ate',
+				options: ['eaten', 'ate', 'eats'],
+			},
+		],
+	},
+	{
+		id: 5,
+		name: 'Past Simple',
+		embed: 'Ndj3E8DUFCk',
+		questions: [
+			{
+				id: 1,
+				question: 'She ___ a letter yesterday.',
+				answer: 'wrote',
+				options: ['write', 'wrote', 'written'],
+			},
+			{
+				id: 2,
+				question: 'They ___ dinner at 8 PM.',
+				answer: 'had',
+				options: ['have', 'had', 'has'],
+			},
+		],
+	},
+	{
+		id: 6,
+		name: 'Past Perfect',
+		embed: 'X6Im6WjofTo',
+		questions: [
+			{
+				id: 1,
+				question: 'By the time we arrived, they ___ left.',
+				answer: 'had',
+				options: ['have', 'had', 'has'],
+			},
+			{
+				id: 2,
+				question: 'She ___ finished the book before the exam.',
+				answer: 'had',
+				options: ['has', 'have', 'had'],
+			},
+		],
+	},
+	{
+		id: 7,
+		name: 'Present Perfect Continuous',
+		embed: 'hGz_Oc4OfiY',
+		questions: [
+			{
+				id: 1,
+				question: 'She ___ for three hours.',
+				answer: 'has been studying',
+				options: ['has studied', 'has been studying', 'studies'],
+			},
+			{
+				id: 2,
+				question: 'They ___ for the bus since morning.',
+				answer: 'have been waiting',
+				options: ['have waited', 'have been waiting', 'waits'],
+			},
+		],
+	},
+	{
+		id: 8,
+		name: 'Future Continuous',
+		embed: '6-hxEmwxh2o',
+		questions: [
+			{
+				id: 1,
+				question: 'This time tomorrow, she ___ to New York.',
+				answer: 'will be flying',
+				options: ['will fly', 'will be flying', 'flies'],
+			},
+			{
+				id: 2,
+				question: 'At 8 PM, they ___ dinner.',
+				answer: 'will be having',
+				options: ['will have', 'will be having', 'has'],
+			},
+		],
+	},
+	{
+		id: 9,
+		name: 'Plans',
+		embed: 'uR2j2Ah1E5U',
+		questions: [
+			{
+				id: 1,
+				question: 'I ___ to the concert next Friday.',
+				answer: 'am going',
+				options: ['will go', 'am going', 'go'],
+			},
+			{
+				id: 2,
+				question: 'She ___ a new course this summer.',
+				answer: 'is starting',
+				options: ['will start', 'is starting', 'starts'],
+			},
+		],
+	},
+	{
+		id: 10,
+		name: 'Article and Nouns',
+		embed: '49RNu3zbR5U',
+		questions: [
+			{
+				id: 1,
+				question: 'I saw ___ elephant at the zoo.',
+				answer: 'an',
+				options: ['a', 'an', 'the'],
+			},
+			{
+				id: 2,
+				question: 'She is ___ best student in the class.',
+				answer: 'the',
+				options: ['a', 'an', 'the'],
+			},
+		],
+	},
+	{
+		id: 11,
+		name: 'Modal Verbs',
+		embed: 'jTe8lbtdQuE',
+		questions: [
+			{
+				id: 1,
+				question: 'You ___ finish your homework before going out.',
+				answer: 'must',
+				options: ['must', 'can', 'might'],
+			},
+			{
+				id: 2,
+				question: 'She ___ speak three languages fluently.',
+				answer: 'can',
+				options: ['must', 'can', 'should'],
+			},
+		],
+	},
+	{
+		id: 12,
+		name: 'Future in the Past',
+		embed: 'JQgZpGGfFwM',
+		questions: [
+			{
+				id: 1,
+				question: 'He said he ___ visit us next week.',
+				answer: 'would',
+				options: ['will', 'would', 'shall'],
+			},
+			{
+				id: 2,
+				question: 'She promised she ___ finish the work by Monday.',
+				answer: 'would',
+				options: ['will', 'would', 'should'],
+			},
+		],
+	},
+	{
+		id: 13,
+		name: 'Future Perfect',
+		embed: 'CqLxR1AFOzg',
+		questions: [
+			{
+				id: 1,
+				question: 'By next year, she ___ her degree.',
+				answer: 'will have completed',
+				options: ['will complete', 'will have completed', 'completes'],
+			},
+			{
+				id: 2,
+				question: 'By 10 PM, they ___ their homework.',
+				answer: 'will have finished',
+				options: ['will finish', 'will have finished', 'finishes'],
+			},
+		],
+	},
+	{
+		id: 14,
+		name: 'Future Perfect Continuous',
+		embed: 'UbQ6nQqXy64',
+		questions: [
+			{
+				id: 1,
+				question: 'By 2025, she ___ in this company for five years.',
+				answer: 'will have been working',
+				options: ['will work', 'will have been working', 'works'],
+			},
+			{
+				id: 2,
+				question: 'By next month, they ___ together for a decade.',
+				answer: 'will have been living',
+				options: ['will live', 'will have been living', 'lives'],
+			},
+		],
+	},
+	{
+		id: 15,
+		name: 'Present Perfect',
+		embed: 'JcTqVjK3Oao',
+		questions: [
+			{
+				id: 1,
+				question: 'She ___ just finished her homework.',
+				answer: 'has',
+				options: ['has', 'have', 'had'],
+			},
+			{
+				id: 2,
+				question: 'They ___ visited Paris twice.',
+				answer: 'have',
+				options: ['has', 'have', 'was'],
+			},
+		],
+	},
+	{
+		id: 16,
+		name: 'Past Continuous',
+		embed: 'uWUwZv8MhDg',
+		questions: [
+			{
+				id: 1,
+				question: 'I ___ watching TV when she called me.',
+				answer: 'was',
+				options: ['was', 'were', 'is'],
+			},
+			{
+				id: 2,
+				question: 'They ___ playing football at 4 PM yesterday.',
+				answer: 'were',
+				options: ['was', 'were', 'is'],
+			},
+		],
+	},
+	{
+		id: 17,
+		name: 'Past Perfect Continuous',
+		embed: 'g5fM4PvG9rA',
+		questions: [
+			{
+				id: 1,
+				question:
+					'She ___ working for that company for five years before she left.',
+				answer: 'had been',
+				options: ['has been', 'had been', 'was'],
+			},
+			{
+				id: 2,
+				question:
+					'They ___ playing football all afternoon when it started to rain.',
+				answer: 'had been',
+				options: ['have been', 'had been', 'were'],
+			},
+		],
+	},
+	{
+		id: 18,
+		name: 'Future Simple',
+		embed: 'oW2lZbmQYtY',
+		questions: [
+			{
+				id: 1,
+				question: 'I ___ go to the cinema tomorrow.',
+				answer: 'will',
+				options: ['will', 'am', 'is'],
+			},
+			{
+				id: 2,
+				question: 'They ___ arrive at 7 PM.',
+				answer: 'will',
+				options: ['will', 'is', 'was'],
+			},
+		],
+	},
+]
