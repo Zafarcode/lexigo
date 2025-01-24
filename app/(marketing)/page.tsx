7a37be7f9adbb34248ce00ef17ebd8bf7e4f2ec1
shakhbozbekusmonov <shakhbozbek.usmonov@gmail.com>
2025-01-24 11:52:32 +0500

    add: merged development codes

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,6 +3,7 @@ import Courses from '@/components/marketing/home/courses'
 import CTA from '@/components/marketing/home/cta'
 import Hero from '@/components/marketing/home/hero'
 import Testimonials from '@/components/marketing/home/testimonials'
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import React from 'react'
