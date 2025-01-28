bb1ed410c7c8c28e98c731d560eb766cd302fd0e
shakhbozbekusmonov <shakhbozbek.usmonov@gmail.com>
2025-01-28 23:42:03 +0500

    fix: finish quiz server error

--- components/vocabulary/finish-quiz/index.tsx
+++ components/vocabulary/finish-quiz/index.tsx
@@ -1,24 +1,28 @@
 'use client'
-import React, { useEffect, useState, useCallback, useRef, MouseEvent } from 'react'
-import { Card } from '@/components/ui/card'
-import { Heart, X, Volume2 } from 'lucide-react'
-import Link from 'next/link'
-import { Progress } from '@/components/ui/progress'
 import { Button } from '@/components/ui/button'
-import { FinishQuizProps } from '@/types'
-import CelebrationDialog from '../celebration-dialog'
-import useTTS from '@/hooks/useTTS'
-import Image from 'next/image'
+import { Card } from '@/components/ui/card'
+import { Progress } from '@/components/ui/progress'
 import { congratulationIconsData } from '@/constants/congratulationIcons'
-import Keyboard from './keyboard'
+import useTTS from '@/hooks/useTTS'
+import { FinishQuizProps } from '@/types'
+import { Heart, X } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import CelebrationDialog from '../celebration-dialog'
+import Keyboard from '@/components/vocabulary/finish-quiz/keyboard'
 
 const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 	const [currentIndex, setCurrentIndex] = useState(0)
 	const [lossCount, setLossCount] = useState(5)
 	const [progress, setProgress] = useState(0)
-	const [gameState, setGameState] = useState<'playing' | 'continue' | 'lose' | 'win' | 'end' | 'start'>('playing')
+	const [gameState, setGameState] = useState<
+		'playing' | 'continue' | 'lose' | 'win' | 'end' | 'start'
+	>('playing')
 	const [inputSpaces, setInputSpaces] = useState<string[]>([])
-	const [clickedLetters, setClickedLetters] = useState<Record<string, boolean>>({})
+	const [clickedLetters, setClickedLetters] = useState<Record<string, boolean>>(
+		{}
+	)
 	const [showCongratulations, setShowCongratulations] = useState(false)
 	const successAudioRef = useRef<HTMLAudioElement | null>(null)
 	const loseAudioRef = useRef<HTMLAudioElement | null>(null)
@@ -27,27 +31,29 @@ const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 
 	const { handleNormalSpeech } = useTTS()
 
-
 	const initializeWord = useCallback(() => {
 		setInputSpaces(Array(currentWord.length).fill('_'))
 		setClickedLetters({})
 	}, [currentWord])
 
-	const handleWin = useCallback((currentWord: string) => {
-		successAudioRef.current?.play()
-		const isLastWord = currentIndex === options.length - 1
-		onViewed(options[currentIndex].id)
-		if (!isLastWord) {
-			setCurrentIndex(prev => prev + 1)
-			setProgress(((currentIndex + 1) / options.length) * 100)
-			setGameState('continue')
-		} else {
-			setProgress(100)
-			setShowCongratulations(true)
-			setGameState('win')
-			setTimeout(() => setShowCongratulations(false), 3000)
-		}
-	}, [currentIndex, options, onViewed,])
+	const handleWin = useCallback(
+		(currentWord: string) => {
+			successAudioRef.current?.play()
+			const isLastWord = currentIndex === options.length - 1
+			onViewed(options[currentIndex].id)
+			if (!isLastWord) {
+				setCurrentIndex(prev => prev + 1)
+				setProgress(((currentIndex + 1) / options.length) * 100)
+				setGameState('continue')
+			} else {
+				setProgress(100)
+				setShowCongratulations(true)
+				setGameState('win')
+				setTimeout(() => setShowCongratulations(false), 3000)
+			}
+		},
+		[currentIndex, options, onViewed]
+	)
 
 	const handleLoss = useCallback(() => {
 		loseAudioRef.current?.play()
@@ -131,16 +137,21 @@ const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 	}, [])
 
 	function getRandomIcon() {
-		const randomIndex = Math.floor(Math.random() * congratulationIconsData.length);
-		return congratulationIconsData[randomIndex].svgIcon;
+		const randomIndex = Math.floor(
+			Math.random() * congratulationIconsData.length
+		)
+		return congratulationIconsData[randomIndex].svgIcon
 	}
 
-	const randomIcon = getRandomIcon();
+	const randomIcon = getRandomIcon()
 
 	return (
 		<div className='w-full lg:max-w-5xl mx-auto flex flex-col items-center gap-5 p-3 sm:p-5'>
-			<CelebrationDialog isOpen={showCongratulations} onClose={() => setShowCongratulations(false)} />
-				{/* Progress */}
+			<CelebrationDialog
+				isOpen={showCongratulations}
+				onClose={() => setShowCongratulations(false)}
+			/>
+			{/* Progress */}
 			{['playing', 'continue', 'win', 'lose'].includes(gameState) && (
 				<div className='w-full flex justify-between items-center gap-2'>
 					<Link href={`/dashboard/vocabulary/${slug}`}>
@@ -187,8 +198,19 @@ const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 							<div className=' w-[90%] sm:w-[70%] flex justify-between items-center'>
 								<div className=' flex justify-center items-center gap-3 sm:gap-5'>
 									<div className=' w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] rounded-full bg-white flex justify-center items-center p-2'>
-										<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
-											<path fill="rgba(88,204,2,255)" d="M40.6 12.1L17 35.7 7.4 26.1 4.6 29 17 41.3 43.4 14.9z" className=' text-duolingoGreen'></path>
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											x='0px'
+											y='0px'
+											width='48'
+											height='48'
+											viewBox='0 0 48 48'
+										>
+											<path
+												fill='rgba(88,204,2,255)'
+												d='M40.6 12.1L17 35.7 7.4 26.1 4.6 29 17 41.3 43.4 14.9z'
+												className=' text-duolingoGreen'
+											></path>
 										</svg>
 									</div>
 									<span className=' text-2xl sm:text-3xl font-bold text-green-500'>
@@ -214,7 +236,8 @@ const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 								<span className='font-bold'>Hint:</span> {currentHint}
 							</p>
 							<p className='flex gap-2 text-base sm:text-lg'>
-								<span className='font-bold'>This word:</span> {currentWord.toUpperCase()}
+								<span className='font-bold'>This word:</span>{' '}
+								{currentWord.toUpperCase()}
 							</p>
 							<Keyboard
 								clickedLetters={clickedLetters}
@@ -230,8 +253,24 @@ const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 							<div className=' w-[90%] sm:w-[70%] flex justify-between items-center'>
 								<div className=' flex justify-center items-center gap-3 sm:gap-5'>
 									<div className=' w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] rounded-full bg-white flex justify-center items-center p-2'>
-										<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
-											<path fill="#ec4899" d="M21.5 4.5H26.501V43.5H21.5z" transform="rotate(45.001 24 24)"></path><path fill="#ec4899" d="M21.5 4.5H26.5V43.501H21.5z" transform="rotate(135.008 24 24)"></path>
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											x='0px'
+											y='0px'
+											width='48'
+											height='48'
+											viewBox='0 0 48 48'
+										>
+											<path
+												fill='#ec4899'
+												d='M21.5 4.5H26.501V43.5H21.5z'
+												transform='rotate(45.001 24 24)'
+											></path>
+											<path
+												fill='#ec4899'
+												d='M21.5 4.5H26.5V43.501H21.5z'
+												transform='rotate(135.008 24 24)'
+											></path>
 										</svg>
 									</div>
 									<span className=' text-2xl sm:text-3xl text-left font-bold text-pink-500'>
@@ -287,27 +326,55 @@ const FinishQuiz = ({ options, onViewed, slug }: FinishQuizProps) => {
 					<React.Fragment>
 						<div className=' flex justify-center items-center gap-5 flex-col'>
 							<div className=' w-full'>
-								<Image src={randomIcon} alt='congratulation' width={200} height={50} className=' w-[150px] sm:w-full object-contain mx-auto' />
+								<Image
+									src={randomIcon}
+									alt='congratulation'
+									width={200}
+									height={50}
+									className=' w-[150px] sm:w-full object-contain mx-auto'
+								/>
 							</div>
 							<div className='  flex justify-center items-center gap-5 flex-col'>
-								<p className=' text-2xl sm:text-3xl font-semibold text-primary'>History is complete!</p>
+								<p className=' text-2xl sm:text-3xl font-semibold text-primary'>
+									History is complete!
+								</p>
 								<div className=' flex justify-center items-center gap-3 flex-col sm:flex-row'>
 									<div className=' w-[180px] sm:w-[200px] p-1 bg-yellow-500 flex flex-col justify-center items-center gap-3 rounded-xl'>
 										<p className=' text-white font-medium'>Experience points</p>
 										<div className=' flex justify-center items-center gap-1 bg-white w-full h-full rounded-xl py-5'>
 											<div className=' w-[30px] overflow-hidden'>
-												<Image src={'https://d35aaqx5ub95lt.cloudfront.net/images/icons/f5358b2d4087a109790fc809eedc08c5.svg'} alt='pointIcon' width={20} height={20} className=' object-contain' />
+												<Image
+													src={
+														'https://d35aaqx5ub95lt.cloudfront.net/images/icons/f5358b2d4087a109790fc809eedc08c5.svg'
+													}
+													alt='pointIcon'
+													width={20}
+													height={20}
+													className=' object-contain'
+												/>
 											</div>
-											<p className=' font-semibold text-lg text-yellow-500'>5</p>
+											<p className=' font-semibold text-lg text-yellow-500'>
+												5
+											</p>
 										</div>
 									</div>
 									<div className='  w-[180px] sm:w-[200px] p-1 bg-green-500 flex flex-col justify-center items-center gap-3 rounded-xl'>
 										<p className=' text-white font-medium'>Great</p>
 										<div className=' flex justify-center items-center gap-2 bg-white w-full h-full rounded-xl py-5'>
 											<div className=' w-[30px] overflow-hidden'>
-												<Image src={'https://d35aaqx5ub95lt.cloudfront.net/images/icons/9ace13520a375f5661415ff7d470f243.svg'} alt='pointIcon' width={30} height={30} className=' object-contain' />
+												<Image
+													src={
+														'https://d35aaqx5ub95lt.cloudfront.net/images/icons/9ace13520a375f5661415ff7d470f243.svg'
+													}
+													alt='pointIcon'
+													width={30}
+													height={30}
+													className=' object-contain'
+												/>
 											</div>
-											<p className=' font-semibold text-lg text-green-500'>100 %</p>
+											<p className=' font-semibold text-lg text-green-500'>
+												100 %
+											</p>
 										</div>
 									</div>
 								</div>
